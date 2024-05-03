c268c24a2c4a64435c737e8fe74048cb3434d14b
十一月 <syybili@163.com>
2024-05-03 17:11:06 +0800

    上传文件

---
+++ module/yunbei_sign.js
@@ -0,0 +1,12 @@
+module.exports = (query, request) => {
+  const data = {
+    type: '0',
+  }
+  return request('POST', `https://music.163.com/api/point/dailyTask`, data, {
+    crypto: 'weapi',
+    cookie: query.cookie,
+    ua: query.ua || '',
+    proxy: query.proxy,
+    realIP: query.realIP,
+  })
+}
